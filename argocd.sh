df55594f60e308f5eee3418c1204367fd43e9490
chinni venkatapavan <chinnivenkatapavan@gmil.com>
2025-03-03 06:43:54 +0530

    load-file using helm

--- argocd.sh
+++ argocd.sh
@@ -1,3 +1,7 @@
+# Getting Ip address to argocd-server by using shell scripting
 argocd_ip=$(kubectl get svc -n argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# Getting Password to argocd by using shell scripting
 argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
+
+#login the argocd by using shell scripting
 argocd login ${argocd_ip} --insecure --username admin --password ${argocd_password}
